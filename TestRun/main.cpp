97f5a5048b15807173755d6b7e32bc4748d60306
Lapryo <132312981+Lapryo@users.noreply.github.com>
2023-05-01 17:06:06 -0700

    Add files via upload

---
+++ TestRun/main.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+#include<array>
+using namespace std;
+string user;
+string pass;
+
+string userInfo[2][5] = {
+    {"Lapryo", "lapryo99", "User3", "User4", "User5"},
+    {"lapryorocks", "iheartpornhub", "c", "d", "e"}
+};
+
+void searchUserInfo(string username, string password, string success, string errUser, string errPass) {
+    bool found;
+    int id;
+    for (int USERS = 0; USERS < 5; USERS++) {
+        if (username == userInfo[0][USERS]) {
+            found = true;
+            id = USERS;
+            break;
+        }
+    }
+    if (found == true) {
+        if (password == userInfo[1][id]) {
+            cout << success << endl;
+        } else {
+            cout << errPass << endl;
+        }
+    } else {
+        cout << errUser << endl;
+    }
+}
+
+int main() {
+
+    cout<<endl<<"Username: ";
+    cin>>user;
+    cout<<endl<<"Password: ";
+    cin>>pass;
+    cout<<endl;
+    system("pause");
+    cout<<endl;
+
+    searchUserInfo(user, pass, "Login success!", "Unknown user.", "Found user but incorrect password.");
+    cout<<endl;
+    system("pause");
+    cout<<endl;
+
+    return 0;
+}
